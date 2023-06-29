4e4c57e9eb9eb596d4c59c1932331f3d8f8a73ca
Sachindu Srilal <lal95.ac@gmail.com>
2023-06-29 22:11:04 +0530

    Finalize the CoursesHttpController class

--- back-end-nodeJs/src/api/CoursesHttpController.ts
+++ back-end-nodeJs/src/api/CoursesHttpController.ts
@@ -1,3 +1,31 @@
 import express from "express";
 import {datasource} from "../db/dbcp";
 
+export const course=express.Router();
+
+type Course= {
+    "id":string,
+    "description":string,
+    "duration":number
+}
+
+course.get('/',async (req, res)=>{
+    const student=await datasource.query('SELECT * FROM course ');
+    res.json(course);
+});
+
+course.delete('/:id',async (req, res)=>{
+    const result=await datasource.query('DELETE FROM course WHERE id=?',[req.params.id])
+    res.sendStatus(result.affectedRows ? 204:404);
+});
+
+course.patch('/:id',async (req, res)=>{
+    const course=req.body as Course;
+    if (!course.description?.trim()){
+        res.sendStatus(400);
+        return;
+    }
+    const result = await datasource.query("UPDATE course SET description = ?, duration =? WHERE id = ?",
+        [course.description,course.duration, req.params.id]);
+    res.sendStatus(result.affectedRows ? 204 : 404);
+});
